669cf994de80ace108f2995a8cbc2cf576f2d3bd
mjsqu <mj2.squ@gmail.com>
2023-06-04 15:40:08 +1200

    Update run_db2_query.py

--- run_db2_query.py
+++ run_db2_query.py
@@ -4,9 +4,9 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 result = sock.connect_ex(('ibm_db2',50000))
 if result == 0:
-   print "Port is open"
+   print("Port is open")
 else:
-   print "Port is not open"
+   print("Port is not open")
 sock.close()
 
 db2_config = "ibm_db_sa://{}:{}@{}:{}/{}".format(
