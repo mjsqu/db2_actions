205c8963e524ef9cc556e9954686631f187cdc99
mjsqu <mj2.squ@gmail.com>
2023-06-04 15:27:17 +1200

    Update run_db2_query.py

--- run_db2_query.py
+++ run_db2_query.py
@@ -1,4 +1,13 @@
 import sqlalchemy
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+result = sock.connect_ex(('ibm_db2',50000))
+if result == 0:
+   print "Port is open"
+else:
+   print "Port is not open"
+sock.close()
 
 db2_config = "ibm_db_sa://{}:{}@{}:{}/{}".format(
         "DB2INST1",
